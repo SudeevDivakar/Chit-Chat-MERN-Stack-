d4918eb78b64bc49e68d34b694fd483b584df654
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-02-11 10:07:48 +0530

    Add Frontend Routes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,27 @@
 //Require Express
-const express = require('express');
+const express = require("express");
 
 //Require environment variables
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
+
+//Require cors
+const cors = require("cors");
 
 //Start Express App
 const app = express();
 
+//Configure middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.urlencoded({ extended: false }));
 
 //Start Express server
 app.listen(process.env.PORT, () => {
-    console.log(`SERVER STARTED ON PORT ${process.env.PORT}`);
+  console.log(`SERVER STARTED ON PORT ${process.env.PORT}`);
 });
